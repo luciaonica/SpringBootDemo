ae794362303cdc4e24347bc3b604e8e0d7b07f6d
lucia <lucia12onica@gmail.com>
2023-06-06 15:45:17 -0500

    first commit

---
+++ src/main/java/com/example/DemoJenkins/DemoJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.example.DemoJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
